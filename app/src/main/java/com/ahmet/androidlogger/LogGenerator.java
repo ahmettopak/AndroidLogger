a3edb2d70f9b44e202329a5eafc3a67b54b25d6a
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2025-01-27 21:45:09 +0300

    log generator edit

--- app/src/main/java/com/ahmet/androidlogger/LogGenerator.java
+++ app/src/main/java/com/ahmet/androidlogger/LogGenerator.java
@@ -8,6 +8,7 @@ import java.util.Random;
  * Author: Ahmet TOPAK
  * Since: 1/27/2025
  */
+
 public class LogGenerator {
     private static final String[] LOG_MESSAGES = {
             "User logged in.",
@@ -39,22 +40,30 @@ public class LogGenerator {
                 try {
                     // Rastgele bir log seviyesi ve mesaj oluştur
                     int logLevel = random.nextInt(4);
-                    String message = LOG_MESSAGES[random.nextInt(LOG_MESSAGES.length)];
 
-                    // Seviyeye göre loglama yap
-                    switch (logLevel) {
-                        case 0:
-                            logManager.logInfo(message);
-                            break;
-                        case 1:
-                            logManager.logDebug(message);
-                            break;
-                        case 2:
-                            logManager.logWarning(message);
-                            break;
-                        case 3:
-                            logManager.logError(message);
-                            break;
+                    boolean isMetricLog = random.nextBoolean(); // Grafik verisi mi, normal log mu
+
+                    if (isMetricLog) {
+                        String metricLog = generateMetricLog();
+                        logManager.logInfo(metricLog); // Metric logları bilgi seviyesinde loglanıyor
+                    } else {
+                        String message = LOG_MESSAGES[random.nextInt(LOG_MESSAGES.length)];
+
+                        // Seviyeye göre loglama yap
+                        switch (logLevel) {
+                            case 0:
+                                logManager.logInfo(message);
+                                break;
+                            case 1:
+                                logManager.logDebug(message);
+                                break;
+                            case 2:
+                                logManager.logWarning(message);
+                                break;
+                            case 3:
+                                logManager.logError(message);
+                                break;
+                        }
                     }
 
                     // Bir saniye bekle
@@ -71,5 +80,17 @@ public class LogGenerator {
     // Durdurma işlemi
     public void stop() {
         isLoggingActive = false;
+    }
+
+    // Grafik verisi oluşturma metodu
+    private String generateMetricLog() {
+        int current = random.nextInt(100); // 0-99 arası değer
+        int bus = random.nextInt(100);
+        int volt = random.nextInt(100);
+        int rpm = random.nextInt(5000); // 0-4999 arası değer
+        int targetSpeed = random.nextInt(5000); // 0-4999 arası değer
+        int metricNumber = random.nextInt(10); // 0-4999 arası değer
+
+        return String.format("Motor %d - Current: %d, Bus: %d, Volt: %d, RPM: %d, TargetSpeed: %d", metricNumber , current, bus, volt, rpm , targetSpeed);
     }
 }
